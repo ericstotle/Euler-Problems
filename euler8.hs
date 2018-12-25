7c461b88a6bbf3327f404e5b38c815043e5869b3
ericstotle <43819683+ericstotle@users.noreply.github.com>
2018-12-25 22:05:36 +0100

    Delete euler8.hs

--- euler8.hs
+++
@@ -1,7 +0,0 @@
-pythagorean_triplets =
-  [ (a,b,c) | a <- [1..], b <- [1..a], c <- [1..b], a + b + c == 1000, a^2 == b^2 + c^2 ]
-
-main :: IO ()
-main = do
-  let answer = (\(a,b,c) -> a * b * c) $ head pythagorean_triplets
-  print answer
